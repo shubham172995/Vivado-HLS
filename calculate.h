f2c1c7148c373a58aa61120d7ede91b277c63eb1
shubhsy <32479973+shubhsy@users.noreply.github.com>
2020-01-30 18:44:15 +0530

    Update calculate.h

--- calculate.h
+++ calculate.h
@@ -3,7 +3,10 @@
 
 #include "systemc.h"
 #include "hls_math.h"
-#include "ap_int.h"
+#include "stdlib.h"
+#include <string>
+//#include <sstream>
+
 
 SC_MODULE (calculation){
 	sc_in_clk clock;
@@ -14,23 +17,42 @@ SC_MODULE (calculation){
 	sc_out<sc_uint<4> > dout_words;
 	sc_out<sc_uint<32> > ctrl;
 
-	ap_uint<256> data;
+	std::string data;
+	sc_uint<128> dat;
+	sc_uint<4> width;
 
 	void datagen(){
-		while(1){
-			wait();
-			din_words=11;
-			din=0;
-			if(data==0)
-				break;
+		//while(1){
+			//wait();
+			//din_words=11;
+			//din=0;
+			width=10;
+			std::string t;
+			dat=0x0;
+			int l=data.length();
+			/*if(data.length()==0)
+				break;*/
 			/*for(int i=0;i<128;i++)
 				din[i]=0;*/
-			for(int i=0;i<(din_words*8);i++){
-				int temp=data%2;
-				data/=2;
-				din=din+(temp*pow(2, i));
+
+			if(l>=20){
+				t=data.substr(l-20, 20);
+				data=data.substr(0, l-20);
 			}
-		}
+			else{
+				t=data.substr(0, l);
+				data="";
+			}
+			/*for(int i=t.length();i>=0;i--){
+				int temp=t[i]-'0';
+				dat=(dat+(temp*pow(2, i)));
+			}*/
+			//std::stringstream str;
+			//str<<t;
+			//str>>std::hex>>dat;
+			dat=strtol (t.c_str(),NULL,16);
+			din.write(dat);
+		//}
 	}
 
 	SC_CTOR(calculation)
@@ -42,18 +64,18 @@ SC_MODULE (calculation){
 	 din_tlast("din_tlast"),
 	 din_tvalid("din_tvalid")
 	{
-		din_words=11;
-		dout_words=11;
-		ctrl=4233465604;
-		din=0;
-		din_tvalid=1;
-		din_tlast=0;
-		data=0x56FCEA2B05F4059A53A4C646F952BB13ADD832CA95EE819A76;
+		din_words.write(11);
+		dout_words.write(11);
+		ctrl.write(0x048F55FC);
+		din.write(0);
+		din_tvalid.write(1);
+		din_tlast.write(0);
+		data="56FCEA2B05F4059A53A4C646F952BB13ADD832CA95EE819A76";
 		for(int i=0;i<3;i++){
 			if(i==2)
-				din_tlast=1;
+				din_tlast.write(1);
 			SC_METHOD(datagen);
-			sensitive<<clock.pos();
+			//sensitive<<clock.pos();
 		}
 			//SC_CTHREAD(datagen, clock.pos());
 	}
